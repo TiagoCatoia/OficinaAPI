100323cb8288eddf9e369d8a29041239ba9cdc9b
Tiago Catoia <tiagocsz@hotmail.com>
2024-10-03 13:54:52 -0300

    feat: inicializar projeto Spring Boot com configuração básica

---
+++ src/main/java/com/example/ConcertoDeCarro/ConcertoDeCarroApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConcertoDeCarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertoDeCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertoDeCarroApplication.class, args);
+	}
+
+}
