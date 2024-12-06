3e9f482aa244ef071cd18b78b4c8e561ffcec15e
Tiago Catoia <tiagocsz@hotmail.com>
2024-12-05 23:02:46 -0300

    feat: add token-based authentication with Spring Security

---
+++ src/main/java/com/example/Oficina/usuario/DadosAutenticacao.java
@@ -0,0 +1,11 @@
+package com.example.Oficina.usuario;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosAutenticacao(
+        @NotBlank
+        String login,
+
+        @NotBlank
+        String senha
+) {}
